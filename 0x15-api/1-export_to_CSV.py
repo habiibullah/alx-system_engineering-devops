6fbbd86719693acbc6676407ad828517667ff919
habiibullah <ajiboyhabiib@gmail.com>
2023-04-25 10:15:35 +0000

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-"""
-Request from API; Return TODO list progress given employee ID
-Export this data to CSV
-"""
+"""fetching json data from an api"""
+
 import csv
 import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    """return API data"""
-    users = requests.get("http://jsonplaceholder.typicode.com/users")
-    for user in users.json():
-        if user.get('id') == int(argv[1]):
-            USERNAME = (user.get('username'))
-            break
-    TASK_STATUS_TITLE = []
-    todos = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for todo in todos.json():
-        if todo.get('userId') == int(argv[1]):
-            TASK_STATUS_TITLE.append((todo.get('completed'), todo.get('title')))
+    users_id = argv[1]
+    users_url = "https://jsonplaceholder.typicode.com/users/" + users_id
+    users_dict = requests.get(users_url).json()
+    users_name = users_dict.get("username")
+    users_todo = requests.get("{}/todos".format(users_url))
+    users_todo = users_todo.json()
+    file_names = users_id + ".csv"
 
-    """export to csv"""
-    filename = "{}.csv".format(argv[1])
-    with open(filename, "w") as csvfile:
-        fieldnames = ["USER_ID", "USERNAME",
-                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
-                                quoting=csv.QUOTE_ALL)
-        for task in TASK_STATUS_TITLE:
-            writer.writerow({"USER_ID": argv[1], "USERNAME": USERNAME,
-                             "TASK_COMPLETED_STATUS": task[0],
-                             "TASK_TITLE": task[1]})
+    with open(file_names, 'w') as csvfile:
+        for item in users_todo:
+            csvfile.write('"{}","{}","{}","{}"\n'.format(item.get(
+                "userId"), users_name, item.get("completed"),
+                item.get("title")))
+
