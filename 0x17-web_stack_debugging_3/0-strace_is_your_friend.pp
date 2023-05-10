c5408ec2794ace44cbc9de74cdf9d31f19504046
habiibullah <ajiboyhabiib@gmail.com>
2023-05-10 10:43:39 +0000

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Apache returns 500; use this script to fix typo in config
+
+exec { 'fix-wordpress':
+  command => "/bin/sed -i /var/www/html/wp-settings.php \
+  -e 's/class-wp-locale.phpp/class-wp-locale.php/'"
+}
